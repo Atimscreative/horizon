b64df3343152fb5702ee2d150f654d0b6544c33f
Atimscreative <timilehinsunday144@gmail.com>
2025-03-12 23:18:36 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-teal-600">Click Me</Button>
+    </>
+  );
+}
+
+export default App;
